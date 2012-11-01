8553f421cdebd2fd3e20d0b42504d7a132efce39
Martijn Laarman <Mpdreamz@gmail.com>
2012-11-01 14:22:50 +0100

    plane is sort of where we want it

--- src/libs/skyroads.js
+++ src/libs/skyroads.js
@@ -3,16 +3,17 @@ var geometry, material, mesh;
 
 
 var SkyRoads = (function() {
-    var rotation = {
-        x: 0, y: 0, z: 0
+    var plane = {
+        size : { x: 200, y: 10, z: 200 },
+        rotation : { x: 0, y: 0, z: 0 }
     };
 
     var camera = {
-        rotation : { x:0, y:0, z:0 }
+        position : { x:0, y:0, z:-500 }
     };
 
     return {
-        rotation: rotation,
+        plane: plane,
         camera: camera
     }
 
@@ -20,19 +21,26 @@ var SkyRoads = (function() {
 
 $(function() {
     var gui = new dat.GUI();
-    var cube = gui.addFolder("Cube rotation");
-    cube.add(SkyRoads.rotation, 'x', -180, 180);
-    cube.add(SkyRoads.rotation, 'y', -180, 180);
-    cube.add(SkyRoads.rotation, 'z', -180, 180);
-
-
+    var plane = gui.addFolder("Plane");
+    plane.open();
+    var planeSize = plane.addFolder("Size");
+    planeSize.add(SkyRoads.plane.size, 'x', 0, 1000);
+    planeSize.add(SkyRoads.plane.size, 'y', 0, 1000);
+    planeSize.add(SkyRoads.plane.size, 'z', 0, 1000);
+    planeSize.open();
+    var planeRotation = plane.addFolder("Rotation");
+    planeRotation.add(SkyRoads.plane.rotation, 'x', -180, 180);
+    planeRotation.add(SkyRoads.plane.rotation, 'y', -180, 180);
+    planeRotation.add(SkyRoads.plane.rotation, 'z', -180, 180);
+    planeRotation.open();    
+    
 
     var camera = gui.addFolder("Camera");
-    var cameraRotation = camera.addFolder("Rotation");
+    var cameraRotation = camera.addFolder("Position");
 
-    cameraRotation.add(SkyRoads.camera.rotation, 'y', -180, 180);
-    cameraRotation.add(SkyRoads.camera.rotation, 'z', -180, 180);
-    cameraRotation.add(SkyRoads.camera.rotation, 'x', -180, 180);
+    cameraRotation.add(SkyRoads.camera.position, 'y', -1000, 1000);
+    cameraRotation.add(SkyRoads.camera.position, 'x', -1000, 1000);
+    cameraRotation.add(SkyRoads.camera.position, 'z', -1000, 1000);
 
 
     gui.open();
@@ -51,7 +59,7 @@ function init() {
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry( 200, 10, 200 );
+    geometry = new THREE.CubeGeometry( 1, 1, 1 );
     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
     mesh = new THREE.Mesh( geometry, material );
@@ -69,13 +77,22 @@ function animate() {
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
 
-    mesh.rotation.x = deg2rad(SkyRoads.rotation.x);
-    mesh.rotation.y = deg2rad(SkyRoads.rotation.y);
-    mesh.rotation.z = deg2rad(SkyRoads.rotation.z);
+    mesh.scale.x = SkyRoads.plane.size.x;
+    mesh.scale.y = SkyRoads.plane.size.y;
+    mesh.scale.z = SkyRoads.plane.size.z;
+    mesh.rotation.x = deg2rad(SkyRoads.plane.rotation.x);
+    mesh.rotation.y = deg2rad(SkyRoads.plane.rotation.y);
+    mesh.rotation.z = deg2rad(SkyRoads.plane.rotation.z);
 
-    camera.rotation.x = deg2rad(SkyRoads.camera.rotation.x);
-    camera.rotation.y = deg2rad(SkyRoads.camera.rotation.y);
-    camera.rotation.z = deg2rad(SkyRoads.camera.rotation.z);
+    camera.position.x = SkyRoads.camera.position.x;
+    camera.position.y = SkyRoads.camera.position.y;
+    camera.position.z = SkyRoads.camera.position.z;
+
+    camera.lookAt(mesh.position);
+
+    // camera.target.position.x = 0;
+    // camera.target.position.y = 0;
+    // camera.target.position.z = 0;
 
     renderer.render( scene, camera );
 
