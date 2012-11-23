f626e7cda8200d91bac9d53b3876119b83119f0a
Martijn Laarman <Mpdreamz@gmail.com>
2012-11-23 19:51:28 +0100

    Collision detection reworked. Frontal detection implemented.

--- src/js/vehicle.js
+++ src/js/vehicle.js
@@ -38,9 +38,14 @@ var Vehicle = (function () {
         }
         SkyRoads.vehicle.velocity.y -= SkyRoads.world.gravity * SkyRoads.delta;
 
-        if (hasCollisions()) {
+        if ( hasFrontalCollisions() ) {
+            console.log('explosion');
+        }
+        else if (hasLateralCollisions()) {
+
             SkyRoads.vehicle.velocity.x = 0;
         }
+
 
         SkyRoads.vehicle.position.x += SkyRoads.vehicle.velocity.x * SkyRoads.delta;
         SkyRoads.vehicle.position.y += SkyRoads.vehicle.velocity.y * SkyRoads.delta;
@@ -63,15 +68,25 @@ var Vehicle = (function () {
         mesh.scale.z = SkyRoads.vehicle.size.z;
     }
 
-    function hasCollisions() {
-        var originPoint = mesh.position.clone();
-        var direction = -SkyRoads.vehicle.velocity.x / Math.abs(SkyRoads.vehicle.velocity.x) || 1;
-        originPoint = originPoint.subSelf(new THREE.Vector3(SkyRoads.vehicle.size.x / 2 * direction, SkyRoads.vehicle.size.y / 2, 0));
-
+    function hasLateralCollisions() {
+        var boxRadiusX = SkyRoads.vehicle.size.x / 2;
         var directionVector = new THREE.Vector3(SkyRoads.vehicle.velocity.x, 0, 0);
+        return hasCollisions(directionVector, boxRadiusX);
+    }
+
+    function hasFrontalCollisions() {
+        var boxRadiusZ = SkyRoads.vehicle.size.y / 2;
+        var directionVector = new THREE.Vector3(0, 0, -SkyRoads.vehicle.velocity.z);
+        return hasCollisions(directionVector, boxRadiusZ);
+    }
+
+    function hasCollisions(directionVector, boxRadius) {
+        var originPoint = mesh.position.clone();
+        originPoint = originPoint.subSelf(new THREE.Vector3(0, SkyRoads.vehicle.size.y / 2, 0));
+
         var ray = new THREE.Ray(originPoint, directionVector.clone().normalize());
         var collisionResults = ray.intersectObjects(Level.getMeshes());
-        if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() * SkyRoads.delta) {
+        if (collisionResults.length > 0 && (collisionResults[0].distance - boxRadius) < directionVector.length() * SkyRoads.delta) {
             return true;
         }
         return false;
