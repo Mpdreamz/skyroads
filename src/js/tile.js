5d21371701ccf5ca95855e9b29862a2bc4fb401a
Arian <arian@q42.nl>
2012-11-23 14:13:39 +0100

    Don't add more than one tile to a position

--- src/js/tile.js
+++ src/js/tile.js
@@ -24,10 +24,15 @@ var Tile = (function (x, z, h) {
         mesh.scale.z = SkyRoads.cell.size.x;
     }
 
+    function equals(tile) {
+        return ( tile.mesh.position.x == mesh.position.x && tile.mesh.position.z == mesh.position.z );
+    }
+
     init();
 
     return {
         mesh: mesh,
-        update: update
+        update: update,
+        equals: equals
     };
 });
