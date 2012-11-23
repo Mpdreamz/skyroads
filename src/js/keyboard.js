a2c1185ccd406dd967ddb928ed1885ad63e1a9a1
Martijn Laarman <Mpdreamz@gmail.com>
2012-11-23 11:49:42 +0100

    Moved movement logic into vehicle

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -46,21 +46,6 @@ var Keyboard = (function() {
 	};
 
 	var update = function () {
-        if (SkyRoads.keyboard.keyUp) {
-            SkyRoads.vehicle.velocity += SkyRoads.vehicle.acceleration * SkyRoads.delta;
-        }
-        if (SkyRoads.keyboard.keyDown) {
-            SkyRoads.vehicle.velocity -= SkyRoads.vehicle.deceleration * SkyRoads.delta;
-        }
-        SkyRoads.vehicle.velocity = Math.min(SkyRoads.vehicle.velocity, SkyRoads.vehicle.maximumVelocity);
-        SkyRoads.vehicle.velocity = Math.max(0, SkyRoads.vehicle.velocity);
-
-        if (SkyRoads.keyboard.keyLeft) {
-            SkyRoads.vehicle.position.x -= SkyRoads.vehicle.horizontalVelocity * SkyRoads.delta;
-        }
-        if (SkyRoads.keyboard.keyRight) {
-            SkyRoads.vehicle.position.x += SkyRoads.vehicle.horizontalVelocity * SkyRoads.delta;
-        }
     };
 
 	$(init);
--- src/js/vehicle.js
+++ src/js/vehicle.js
@@ -8,17 +8,43 @@ var Vehicle = (function () {
         update();
     }
 
-    var update = function () {
-        SkyRoads.vehicle.position.z -= SkyRoads.vehicle.velocity;
+    function updateState() {
+        if (SkyRoads.keyboard.keyUp) {
+            SkyRoads.vehicle.velocity += SkyRoads.vehicle.acceleration * SkyRoads.delta;
+        }
+        if (SkyRoads.keyboard.keyDown) {
+            SkyRoads.vehicle.velocity -= SkyRoads.vehicle.deceleration * SkyRoads.delta;
+        }
+        SkyRoads.vehicle.velocity = Math.min(SkyRoads.vehicle.velocity, SkyRoads.vehicle.maximumVelocity);
+        SkyRoads.vehicle.velocity = Math.max(0, SkyRoads.vehicle.velocity);
 
+        if (SkyRoads.keyboard.keyLeft) {
+            SkyRoads.vehicle.position.x -= SkyRoads.vehicle.horizontalVelocity * SkyRoads.delta;
+        }
+        if (SkyRoads.keyboard.keyRight) {
+            SkyRoads.vehicle.position.x += SkyRoads.vehicle.horizontalVelocity * SkyRoads.delta;
+        }
+
+        SkyRoads.vehicle.position.z -= SkyRoads.vehicle.velocity;
+    }
+
+    function move() {
         mesh.position.x = SkyRoads.vehicle.position.x;
         mesh.position.y = SkyRoads.vehicle.position.y;
         mesh.position.z = SkyRoads.vehicle.position.z;
+    }
 
+    function scale() {
         mesh.scale.x = SkyRoads.vehicle.size.x;
         mesh.scale.y = SkyRoads.vehicle.size.y;
         mesh.scale.z = SkyRoads.vehicle.size.z;
-    };
+    }
+
+    function update() {
+        updateState();
+        move();
+        scale();
+    }
 
     init();
 
